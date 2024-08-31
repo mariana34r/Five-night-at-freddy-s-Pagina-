5f286a861c296f6077ccc9f0af8ad3f2da85175a
mariana34r <cesaraugustoruedarangel@gmail.com>
2024-08-31 09:21:06 -0500

    feat: :sparkles: Se agrego la seccion tienda y se modificaron secciones
    Se modifico las secciones de la pagina y se agrego la seccion Tienda a la pagina
    
    By @mariana34r

--- tienda.js
+++ tienda.js
@@ -12,7 +12,7 @@ function agregarAlCarrito(nombre, precio, imagen, categoria) {
     actualizarCarrito();
     guardarCarritoEnLocalStorage();
 }
-
+// para eleimnar productos
 function eliminarDelCarrito(nombre) {
     carrito = carrito.filter(producto => producto.nombre !== nombre);
     actualizarCarrito();
